52dd439185a965ba1165f2c982c8a076660e3411
Divyansh Agarwal <divyansh1199@gmail.com>
2020-10-08 01:32:32 +0530

    Add more test for new deck

--- deck_test.go
+++ deck_test.go
@@ -9,4 +9,11 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Errorf("Expected deck length of 16 but got %d", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades but got %v", d[0])
+	}
+	if d[15] != "Four of Clubs" {
+		t.Errorf("Expected last card of Four of Clubs but got %v", d[15])
+	}
 }
